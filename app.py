20e09f57583c6780c3551c1eb355af6dc69e501c
dalred <RcrF63gGG7HpbAT>
2021-12-01 02:36:08 +0300

    first version

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
-
+from functions import read_json
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def page_index():
-    pass
+    tags = []
+    for i in read_json(POST_PATH):
+        for tag in i['content'].split(' '):
+            if '#' in tag:
+                tags.append(tag.lstrip("#"))
+    tags = set(tags)
+    return render_template('index.html', tags=tags)
 
 
-@app.route("/tag")
+@app.route("/tag/", methods=["GET"])
 def page_tag():
-    pass
+    tags = []
+    tag_name = request.args.get('tag')
+    tag = str("#")+tag_name
+    for i in read_json('posts.json'):
+        if tag in i['content']:
+            tags.append(i)
+    return render_template('post_by_tag.html', tags=tags, tag_name=tag_name)
 
 
 @app.route("/post", methods=["GET", "POST"])
@@ -26,6 +36,6 @@ def page_post_create():
 def static_dir(path):
     return send_from_directory("uploads", path)
 
-
-app.run()
+if __name__ == "__main__":
+    app.run('127.0.0.1', 8000)
 
--- functions.py
+++ functions.py
@@ -1,1 +1,4 @@
 import json
+def read_json(name):
+    with open(name, "r", encoding='utf-8') as file:
+        return json.load(file)

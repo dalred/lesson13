9409d1534fc55abb1d119026aec5f61d4d4ae005
dalred <RcrF63gGG7HpbAT>
2021-12-03 03:29:21 +0300

    one more commit change functions

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
-import json, os
-from pathlib import Path
 from flask import Flask, request, render_template, send_from_directory
-from functions import read_json
+from functions import read_json, search_tag, jsondump, view_tag
+
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def page_index():
-    tags = []
-    for i in read_json(POST_PATH):
-        for tag in i['content'].split(' '):
-            if '#' in tag:
-                tags.append(tag.lstrip("#"))
-    tags = set(tags)
+    tags = view_tag(POST_PATH)
     return render_template('index.html', tags=tags)
 
 
 @app.route("/tag/", methods=["GET"])
 def page_tag():
-    tags = []
     tag_name = request.args.get('tag')
-    tag = str("#")+tag_name
-    for i in read_json('posts.json'):
-        if tag in i['content']:
-            tags.append(i)
+    tags = search_tag(tag_name)
     return render_template('post_by_tag.html', tags=tags, tag_name=tag_name)
 
 
@@ -36,16 +27,7 @@ def page_post_create():
         return render_template("post_form.html")
     else:
         file = request.files['picture']
-        path = Path(os.path.abspath(__file__)).parent
-        if not os.path.exists(path.joinpath("uploads", file.filename)):
-            file.save(path.joinpath("uploads/images", file.filename))
-        add_post = {
-            "pic": f"../{UPLOAD_FOLDER}/{file.filename}",
-            "content": request.form.get("content"),
-        }
-        data.append(add_post)
-        with open(POST_PATH, "w", encoding='utf-8') as f:
-            json.dump(data, f, indent=4, ensure_ascii=False)
+        add_post = jsondump(POST_PATH, file, data, UPLOAD_FOLDER, request)
         return render_template("post_uploaded.html", **add_post)
 
 
@@ -53,6 +35,6 @@ def page_post_create():
 def static_dir(path):
     return send_from_directory(UPLOAD_FOLDER, path)
 
+
 if __name__ == "__main__":
     app.run('127.0.0.1', 8000)
-
--- functions.py
+++ functions.py
@@ -1,4 +1,38 @@
-import json
+import json, os
+from pathlib import Path
+
+
 def read_json(name):
     with open(name, "r", encoding='utf-8') as file:
         return json.load(file)
+
+
+def search_tag(tag_name):
+    tags = []
+    tag = str("#") + tag_name
+    for i in read_json('posts.json'):
+        if tag in i['content']:
+            tags.append(i)
+    return tags
+
+def view_tag(POST_PATH):
+    tags = []
+    for i in read_json(POST_PATH):
+        for tag in i['content'].split(' '):
+            if '#' in tag:
+                tags.append(tag.lstrip("#"))
+    tags = set(tags)
+    return tags
+
+def jsondump(POST_PATH, file, data, UPLOAD_FOLDER, request):
+    path = Path(os.path.abspath(__file__)).parent
+    if not os.path.exists(path.joinpath("uploads", file.filename)):
+        file.save(path.joinpath("uploads/images", file.filename))
+    add_post = {
+        "pic": f"../{UPLOAD_FOLDER}/{file.filename}",
+        "content": request.form.get("content"),
+    }
+    data.append(add_post)
+    with open(POST_PATH, "w", encoding='utf-8') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+    return add_post
